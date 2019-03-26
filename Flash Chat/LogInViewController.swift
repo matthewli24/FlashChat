7e4485cd810ecb9872195ede1d2be079a761575d
Matthew Li <matthewli2491@gmail.com>
2019-03-26 11:07:48 -0400

    log in ready

--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -28,7 +28,15 @@ class LogInViewController: UIViewController {
     @IBAction func logInPressed(_ sender: AnyObject) {
 
         //TODO: Log in the user
-        
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
+            (user, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                print("Log in Successful")
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
         
     }
     
